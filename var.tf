d2b900491ecca339405af72a4540d1783d8dfbcc
Mahesh <88389547+Mahesh09494@users.noreply.github.com>
2021-12-23 13:04:03 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,28 @@
+variable "region"{
+
+    default = "us-east-1"
+}
+variable "access_key"{
+
+    default = "ACCESS_KEY"
+}
+variable "secret_key"{
+
+    default = "SECERT_KEY"
+}
+variable "ami"{
+
+   default = "ami-083654bd07b5da81d"
+}
+variable "instance_type"{
+
+   default = "t2.micro"
+}
+variable "name"{
+
+    default = "test"
+}
+variable "bucketname"{
+
+    default = "test949494"
+}
